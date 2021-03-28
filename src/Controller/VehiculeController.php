c1e732feef330c8ed599c1543661a68015ba7b7d
Antoine MARIE <antoine.marie62126@gmail.com>
2021-03-28 18:51:46 +0200

    mise en place du bundle pdf pour les factures

--- src/Controller/VehiculeController.php
+++ src/Controller/VehiculeController.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 
 
@@ -144,5 +146,44 @@ class VehiculeController extends AbstractController
         ]);
     }
 
+
+    /**
+     * @Route("/{id}/facture", name="showfacture", methods={"GET"})
+     */
+    public function showfacture(Vehicule $vehicule)
+    {
+        
+
+        {
+            // Configure Dompdf according to your needs
+            $pdfOptions = new Options();
+            $pdfOptions->set('defaultFont', 'Arial');
+            
+            // Instantiate Dompdf with our options
+            $dompdf = new Dompdf($pdfOptions);
+            
+            // Retrieve the HTML generated in our twig file
+            $html = $this->renderView('vehicule/showfacture.html.twig', [
+                'vehicule' => $vehicule,
+            ]);
+            
+            // Load HTML to Dompdf
+            $dompdf->loadHtml($html);
+            
+            // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+            $dompdf->setPaper('A4', 'portrait');
+    
+            // Render the HTML as PDF
+            $dompdf->render();
+    
+            // Output the generated PDF to Browser (force download)
+            $dompdf->stream("mypdf.pdf", [
+                "Attachment" => true
+            ]);
+        }
+
+
+
+    }
   
 }
